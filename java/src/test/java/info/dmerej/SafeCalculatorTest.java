d13b3e8fb188eff80b1a22c14499ffd073921cf8
Kurnoth <arnaud.kohler@bbox.fr>
2024-11-27 11:46:41 -0100

    SafeCalculatortest written

--- java/src/test/java/info/dmerej/SafeCalculatorTest.java
+++ java/src/test/java/info/dmerej/SafeCalculatorTest.java
@@ -5,6 +5,13 @@ import org.junit.jupiter.api.Test;
 public class SafeCalculatorTest {
   @Test
   void should_not_throw_when_authorized() {
-    // TODO: write a test to demonstrate the bug in SafeCalculator.add()
+    SafeCalculator calculator = new SafeCalculator(() -> true);
+    calculator.add(1, 2);
+  }
+
+  public static void main(String[] args) {
+    SafeCalculatorTest test = new SafeCalculatorTest();
+    test.should_not_throw_when_authorized();
   }
 }
+
